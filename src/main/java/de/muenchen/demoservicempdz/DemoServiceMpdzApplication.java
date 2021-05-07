a5f32bbfa661143568af7d97a224fbeafa739971
xdoo <claus.straube@gmail.com>
2021-05-07 14:33:05 +0200

    init

---
+++ src/main/java/de/muenchen/demoservicempdz/DemoServiceMpdzApplication.java
@@ -0,0 +1,13 @@
+package de.muenchen.demoservicempdz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceMpdzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceMpdzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/muenchen/demoservicempdz/DemoServiceMpdzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.muenchen.demoservicempdz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceMpdzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
